0e442ecd0e66ea5d875c0fe7ee48e90a16e7730a
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 13:11:04 -0300

    add eslint

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,7 +1,3 @@
 export default function HomePage() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
