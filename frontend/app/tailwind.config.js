e862ccc1777a856b57007636a123a1cfda0b0f2e
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 14:04:33 -0300

    config styles

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -3,9 +3,43 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '1xl': '1.375rem',
+      '2xl': '1.5rem',
+      '3xl': '1.625rem',
+      '4xl': '1.75rem',
+      '5xl': '1.875rem',
+      '6xl': '2rem',
+    },
+    screens: {
+      tesm: '414px',
+      tsm: '640px',
+      tmd: '768px',
+      tlg: '1024px',
+      txl: '1280px',
+      big: '1536px',
+      't2xl': '1536px',
+      '2xl': {'max': '1536px'},
+      xl: {'max': '1280px'},
+      lg: {'max': '1024px'},
+      md: {'max': '768px'},
+      sm: {'max': '640px'},
+      esm: {'max': '414px'},
+    },
     colors: {
       Gray: {
         100: '#FCFCFC',
+      },
+      Black: {
+        500: '#040B14',
+      },
+      Green: {
+        100: '#CED55B',
       },
     },
   },
