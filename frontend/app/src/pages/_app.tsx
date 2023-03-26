1c4f949524f57ee874c69836a963b2d3a73eb50e
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 13:07:17 -0300

    start tailwind

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import '../assets/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
