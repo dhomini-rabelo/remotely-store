087a77300aeb4362e63bbdcf519b761c443663ca
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 17:02:26 -0300

    add axios client

---
+++ frontend/app/src/code/settings/main.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const BASE_URL = 'http://localhost:8000/api'
+export const MEDIA_BASE_URL = 'http://localhost:8000/media'
+
+export const client = axios.create({
+  baseURL: BASE_URL,
+}) // with Authentication
+
+export const simpleClient = axios.create({
+  baseURL: BASE_URL,
+}) // without Authentication
+
+export const applicationName = 'REMOTELY'
+
+export const refreshTokenTimeoutInSeconds = 60 * 60 * 23 // 23 hours
