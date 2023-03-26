e1c7582872208c8803a7b2e3f9570fc718aa0064
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 16:34:29 -0300

    up validate_unique

--- backend/src/Fast/forms/validators.py
+++ backend/src/Fast/forms/validators.py
@@ -1,23 +1,25 @@
 # django
 from django.core.validators import validate_slug, validate_unicode_slug, validate_email
 from django.core.exceptions import ValidationError
+
 # others
 from string import ascii_letters, digits, punctuation
 from typing import Any
 from collections.abc import Mapping
 
 
-
-def validate_caracters(text: str, with_accents=True, spaces=True, use_symbols=True, use_numbers=True, use_underline=True):
+def validate_caracters(
+    text: str, with_accents=True, spaces=True, use_symbols=True, use_numbers=True, use_underline=True
+):
     accents = 'áàéèíìóòúùâêîôûãõ' if with_accents else ''
     space = ' ' if spaces else ''
     symbols = punctuation if use_symbols else ''
     underline = "_" if use_underline else ''
     numbers = digits if use_numbers else ''
-    alloweds = symbols + numbers + ascii_letters + accents + space + underline 
+    alloweds = symbols + numbers + ascii_letters + accents + space + underline
     for letter in text.lower():
-       if letter not in alloweds:
-           return False
+        if letter not in alloweds:
+            return False
     return True
 
 
@@ -27,19 +29,19 @@ def validate_for_email(email: str):
         return True
     except ValidationError:
         return False
-    
-    
-def validate_unique(Model, field_name: str, field: Any, use_queryset=False):
-    model = Model.objects if not use_queryset else Model
-    current_fields = model.values_list(field_name, flat=True)
-    
-    if field in current_fields:
-        return False
-    return True
 
 
+def validate_unique(ModelOrQueryset, field_name: str, field: Any, use_queryset=False) -> bool:
+    queryset = ModelOrQueryset.objects if not use_queryset else ModelOrQueryset
+    return queryset.filter(**{field_name: field}).exists()
 
-def validate_for_slug(slug:str):
+
+def is_unique(ModelOrQueryset, field_name: str, field: Any, use_queryset=False) -> bool:
+    queryset = ModelOrQueryset.objects if not use_queryset else ModelOrQueryset
+    return not (queryset.filter(**{field_name: field}).exists())
+
+
+def validate_for_slug(slug: str):
     if '/' in slug:
         return False
     try:
@@ -47,8 +49,7 @@ def validate_for_slug(slug:str):
         validate_unicode_slug(slug)
         return True
     except ValidationError:
-        return False  
-    
+        return False
 
 
 def validate_only_numeric(text: Mapping[str, int]):
@@ -56,5 +57,3 @@ def validate_only_numeric(text: Mapping[str, int]):
         if letter not in list('123456789'):
             return False
     return True
-
-    
