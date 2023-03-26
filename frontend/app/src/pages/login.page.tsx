3805e3c41d9e854d9ae810660cec95e6f4f0da05
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-26 14:04:53 -0300

    add login header

--- frontend/app/src/pages/login.page.tsx
+++ frontend/app/src/pages/login.page.tsx
@@ -1,3 +1,22 @@
+import Image from 'next/image'
+import HelloIcon from '../assets/icons/hello.svg'
+
 export default function LoginPage() {
-  return <h1 className="text-3xl font-bold underline">Login</h1>
+  return (
+    <main className="grow flex justify-center">
+      <div className="w-full max-w-esm px-6 mt-40">
+        <div className="flex items-center gap-x-2 text-1xl">
+          <Image
+            src={HelloIcon}
+            width={22}
+            height={22}
+            alt="ícone de mão acenando"
+          />
+          <h1 className="font-bold">
+            Welcome to <span className="text-Green-100">Remotely.io</span>
+          </h1>
+        </div>
+      </div>
+    </main>
+  )
 }
