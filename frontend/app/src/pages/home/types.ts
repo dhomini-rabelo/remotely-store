f47ffbbe1ea03a19caf25efc816e8823d64eaca0
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-27 20:19:11 -0300

    add initial product interface

---
+++ frontend/app/src/pages/home/types.ts
@@ -0,0 +1,11 @@
+export interface IProductData {
+  id: string
+  name: string
+  image: string
+  description: string
+  rating: number
+  price: {
+    value: number
+    promotional_value: number | null
+  }
+}
