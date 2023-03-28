276a6f740811a8784788bb9816a2c475228325cc
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-03-28 20:07:02 -0300

    add search for cell phone

--- frontend/app/src/pages/home/components/Header/index.tsx
+++ frontend/app/src/pages/home/components/Header/index.tsx
@@ -6,10 +6,37 @@ import { useAtom } from 'jotai'
 import { User } from 'phosphor-react'
 import { Bag } from '../Main/subcomponents/Bag'
 import { currentPageAtom, searchTextAtom } from '../../code/states'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 
 export function Header() {
   const [page, setPage] = useAtom(currentPageAtom)
   const [search, setSearch] = useAtom(searchTextAtom)
+  const [showSearch, setShowSearch] = useState(false)
+  const searchInput = useRef<null | HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (showSearch) {
+      searchInput.current!.focus()
+    }
+  }, [showSearch])
+
+  function doSearch(e: ChangeEvent<HTMLInputElement>) {
+    const searchText = e.currentTarget.value
+    setSearch(e.currentTarget.value)
+    if (searchText !== '' && page !== 'search') {
+      setPage('search')
+    }
+  }
+
+  function searchInCellPhone() {
+    setShowSearch(true)
+  }
+
+  function showOptionsInCellPhone() {
+    if (showSearch && search === '') {
+      setShowSearch(false)
+    }
+  }
 
   return (
     <header className="flex justify-between items-center">
@@ -21,13 +48,18 @@ export function Header() {
         />
       </div>
       <nav className="flex gap-x-3">
-        <div className="rounded-full border border-Gray-300 p-2.5 sm:hidden flex items-center">
+        <div
+          className={`${!showSearch && 'sm:hidden'
+            } rounded-full border border-Gray-300 p-2.5 flex items-center`}
+        >
           <input
             type="text"
-            className="bg-transparent px-1 text-base"
+            className="bg-transparent px-1 text-base w-full"
             placeholder="Buscar..."
             value={search}
-            onChange={(e) => setSearch(e.currentTarget.value)}
+            onBlur={showOptionsInCellPhone}
+            ref={searchInput}
+            onChange={doSearch}
             name="s"
           />
           <Image
@@ -36,24 +68,28 @@ export function Header() {
             alt="ícone de uma lupa grande com cabo pequeno"
           />
         </div>
-        <div
-          onClick={() => setPage('search')}
-          className="rounded-full border border-Gray-300 p-2.5 hidden sm:block"
-        >
-          <Image
-            src={SearchIcon}
-            className="w-[1.5rem] h-[1.5rem] sm:w-[1.375rem] sm:h-[1.375rem]"
-            alt="ícone de uma lupa grande com cabo pequeno"
-          />
-        </div>
-        <a
-          href=""
-          className="rounded-full border border-Gray-300 p-2.5 flex items-end gap-x-2"
-        >
-          <User className="w-[1.5rem] h-[1.5rem] sm:w-[1.375rem] sm:h-[1.375rem] text-Black-500" />
-          <span className="text-lg sm:hidden">Conta</span>
-        </a>
-        <Bag inHeader={true} />
+        {!showSearch && (
+          <>
+            <div
+              onClick={searchInCellPhone}
+              className="rounded-full border border-Gray-300 p-2.5 hidden sm:block"
+            >
+              <Image
+                src={SearchIcon}
+                className="w-[1.5rem] h-[1.5rem] sm:w-[1.375rem] sm:h-[1.375rem]"
+                alt="ícone de uma lupa grande com cabo pequeno"
+              />
+            </div>
+            <a
+              href=""
+              className="rounded-full border border-Gray-300 p-2.5 flex items-end gap-x-2"
+            >
+              <User className="w-[1.5rem] h-[1.5rem] sm:w-[1.375rem] sm:h-[1.375rem] text-Black-500" />
+              <span className="text-lg sm:hidden">Conta</span>
+            </a>
+            <Bag inHeader={true} />
+          </>
+        )}
       </nav>
     </header>
   )
