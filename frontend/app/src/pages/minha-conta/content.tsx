d199e8c2c16d7251f85fb80b5fbf83ca4f92ed60
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-02 23:23:19 -0300

    add my requests

--- frontend/app/src/pages/minha-conta/content.tsx
+++ frontend/app/src/pages/minha-conta/content.tsx
@@ -1,4 +1,4 @@
-import { User } from 'phosphor-react'
+import { Package, User } from 'phosphor-react'
 import { Header } from './components/Header'
 
 export function MyAccount() {
@@ -18,6 +18,24 @@ export function MyAccount() {
           </span>
         </div>
         <h2 className="text-1xl font-bold mt-8">Meus pedidos</h2>
+        <div className="grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 mt-3">
+          <div className="bg-Gray-250 p-3 flex gap-x-1 rounded-lg relative">
+            <Package width={98} height={102} className="text-Black-500" />
+            <div className="grow pt-2 flex flex-col gap-y-1 inter justify-evenly">
+              <span className="text-xs">Data: 12/42/33</span>
+              <span className="text-xs">Itens: 5</span>
+              <span className="text-xs">
+                Total: <span className="text-green-500">R$ 50,00</span>
+              </span>
+              <span className="text-xs">
+                Total: <span className="text-yellow-600">Em análise</span>
+              </span>
+            </div>
+            <strong className="text-Orange-500 absolute top-2 right-2 text-xs inter">
+              #AHBASHBDU23
+            </strong>
+          </div>
+        </div>
       </main>
     </div>
   )
