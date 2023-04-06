1739ec290fd0fe33deabed7ad9739b670ddef417
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-06 20:46:08 -0300

    search for department

--- frontend/app/src/pages/home/components/Main/subcomponents/Search/index.tsx
+++ frontend/app/src/pages/home/components/Main/subcomponents/Search/index.tsx
@@ -8,29 +8,21 @@ export function Search({
   search: string
   productsForBuy: IProductData[]
 }) {
-  const filteredProducts = productsForBuy
-    .filter((product) =>
-      `
-              ${product.name.toLowerCase()}
-              ${product.provider.name.toLowerCase()}
-            `.includes(search.toLowerCase()),
-    )
-    .map((product) => <Product key={product.id} product={product} />)
+  const filteredProducts = productsForBuy.filter((product) =>
+    `
+      ${product.name.toLowerCase()}
+      ${product.department.name.toLowerCase()}
+      ${product.provider.name.toLowerCase()}
+    `.includes(search.toLowerCase()),
+  )
 
   return filteredProducts.length ? (
     <main className="mt-10">
       <h2 className="text-1xl font-bold"> Resultados para {`"${search}"`}</h2>
       <div className="grid grid-cols-3 md:grid-cols-2 gap-x-5 sm:flex sm:flex-col mt-3 gap-y-3">
-        {productsForBuy
-          .filter((product) =>
-            `
-              ${product.name.toLowerCase()}
-              ${product.provider.name.toLowerCase()}
-            `.includes(search.toLowerCase()),
-          )
-          .map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
+        {filteredProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
       </div>
     </main>
   ) : (
