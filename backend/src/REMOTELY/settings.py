79af2d0b4098e354daae3e52bdc7f5a39a93cd8e
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-09 19:50:11 -0300

    upmin

--- backend/src/REMOTELY/settings.py
+++ backend/src/REMOTELY/settings.py
@@ -87,7 +87,7 @@ DATABASES = (
     else {
         'default': dj_database_url.config(
             # Feel free to alter this value to suit your needs.
-            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            default='postgresql://postgres:postgres@localhost:5432/REMOTELY',
             conn_max_age=600,
         )
     }
