3a352d0fc95076f32ca2db4d2ecdf1407b9169ed
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-09 16:53:56 -0300

    add /me tests

---
+++ backend/src/apps/accounts/tests/routes/me_test.py
@@ -0,0 +1,25 @@
+from django.test import Client
+from pytest import mark
+
+from django.urls import reverse
+from tests.fixtures.client import AUTH_USER_EMAIL, AUTH_USER_PASSWORD
+
+
+@mark.django_db
+class TestMeAPI:
+    EMAIL = AUTH_USER_EMAIL
+    PASSWORD = AUTH_USER_PASSWORD
+    ROUTE = reverse('accounts:me')
+
+    def test_login_with_simple_user(self, simple_client: Client):
+        response = simple_client.get(
+            self.ROUTE,
+        )
+        assert response.status_code == 401
+
+    def test_login_with_auth_user(self, simple_client: Client, auth_token: str):
+        response = simple_client.get(
+            self.ROUTE,
+            HTTP_AUTHORIZATION=auth_token,
+        )
+        assert response.status_code == 200
--- backend/src/tests/fixtures/client.py
+++ backend/src/tests/fixtures/client.py
@@ -1,7 +1,8 @@
-from rest_framework.test import APIClient
 from pytest import fixture
+from django.test import Client
 
 from apps.accounts.app.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 AUTH_USER_EMAIL = 'test@test.com'
 AUTH_USER_PASSWORD = 'TEST123'
@@ -12,8 +13,14 @@ def create_auth_user():
     user = User(username=AUTH_USER_EMAIL, email=AUTH_USER_EMAIL)
     user.set_password(AUTH_USER_PASSWORD)
     user.save()
+    return user
 
 
 @fixture(scope='session')
 def simple_client():
-    return APIClient()
+    return Client()
+
+
+@fixture
+def auth_token(create_auth_user):
+    return f'Bearer {str(RefreshToken.for_user(create_auth_user).access_token)}'
