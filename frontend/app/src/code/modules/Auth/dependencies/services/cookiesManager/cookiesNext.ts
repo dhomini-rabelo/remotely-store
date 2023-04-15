7fb26dec91fc7f25a997de16dad90f98f3765d5f
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-15 20:49:43 -0300

    implements CookiesManagerContract with cookies-next

---
+++ frontend/app/src/code/modules/Auth/dependencies/services/cookiesManager/cookiesNext.ts
@@ -0,0 +1,39 @@
+import {
+  CookiesManagerContract,
+  INextResponse,
+} from '../../contracts/cookiesManager'
+import { deleteCookie, getCookie, hasCookie, setCookie } from 'cookies-next'
+
+export class cookiesNext implements CookiesManagerContract {
+  getCookieInClient(key: string) {
+    return getCookie(key)
+  }
+
+  getCookieInServer(key: string, nextResponse: INextResponse) {
+    return getCookie(key, nextResponse)
+  }
+
+  hasCookieInClient(key: string) {
+    return hasCookie(key)
+  }
+
+  hasCookieInServer(key: string, nextResponse: INextResponse) {
+    return hasCookie(key, nextResponse)
+  }
+
+  setCookieInClient(key: string, value: string) {
+    return setCookie(key, value)
+  }
+
+  setCookieInServer(key: string, value: string, nextResponse: INextResponse) {
+    return setCookie(key, value, nextResponse)
+  }
+
+  deleteCookieInClient(key: string) {
+    return deleteCookie(key)
+  }
+
+  deleteCookieInServer(key: string, nextResponse: INextResponse) {
+    return deleteCookie(key, nextResponse)
+  }
+}
