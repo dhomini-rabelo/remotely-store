58310524be132da99289f98b7963b4f489b6b70d
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-16 12:53:29 -0300

    add aws settings

--- backend/src/REMOTELY/settings.py
+++ backend/src/REMOTELY/settings.py
@@ -164,3 +164,16 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+
+# s3
+
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_SIGNATURE_NAME = ('s3v4',)
+# AWS_S3_REGION_NAME = 'sa-east-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
