e459b4dfe5f78bfaeb04072c9072098f18bbda1f
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-04-16 14:10:32 -0300

    rm sqlite

--- backend/src/REMOTELY/settings.py
+++ backend/src/REMOTELY/settings.py
@@ -71,25 +71,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'REMOTELY.wsgi.application'
 
 
-DATABASES = (
-    {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    if DEBUG
-    else {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": config("DB_NAME"),
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PASSWORD"),
-            "HOST": config("DB_HOST"),
-            "PORT": 5432,
-        },
-    }
-)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": 5432,
+    },
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
